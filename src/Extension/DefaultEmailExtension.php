8b20204b5e1db0ce9a4b257801edf90cedb216cb
Alexandre Saelens <alexandre.saelens@dna.co.nz>
2025-01-27 15:08:29 +1300

    Allow to bypass env var

--- src/Extension/DefaultEmailExtension.php
+++ src/Extension/DefaultEmailExtension.php
@@ -4,17 +4,19 @@ namespace DNADesign\SubsitesDefaultEmails\Extension;
 
 use SilverStripe\Control\Email\Email;
 use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Environment;
 use SilverStripe\Core\Extension;
+use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\EmailField;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Subsites\Model\Subsite;
 
 /* Adds a DefaultFromEmail field to the Subsite DataObject */
 
 class DefaultEmailExtension extends Extension
 {
     private static $db = [
-        'DefaultFromEmail' => 'Varchar'
+        'DefaultFromEmail' => 'Varchar',
+        'BypassEnvironment' => 'Boolean'
     ];
 
     /**
@@ -23,16 +25,19 @@ class DefaultEmailExtension extends Extension
     public function updateCMSFields(FieldList $fields)
     {
         $adminEmail = Config::inst()->get(Email::class, 'admin_email') ?? '-- not set --';
+        $envEmail = Environment::getEnv('SS_EMAIL_SEND_ALL_FROM');
+        $finalEmail = $envEmail ?? $adminEmail;
 
         $emailField = EmailField::create('DefaultFromEmail')->setDescription(
             'This field can be used to set the default "From" address for emails sent from this subsite. <br>
-            If not set, defaults to '.$adminEmail
+            If not set, defaults to'.$finalEmail
         );
 
-        if (Subsite::currentSubsite()) {
-            $fields->replaceField('DefaultFromEmail', $emailField);
-        } else {
-            $fields->removeByName('DefaultFromEmail');
+        $fields->addFieldToTab('Root.Main', $emailField, 'Language');
+
+        if ($envEmail) {
+            $override = CheckboxField::create('BypassEnvironment')->setDescription('Bypass SS_EMAIL_SEND_ALL_FROM ('.$envEmail.') and use "Default From Email" on this subsite');
+            $fields->addFieldToTab('Root.Main', $override, 'Language');
         }
     }
 }
--- src/Extension/MailerSubscriberExtension.php
+++ src/Extension/MailerSubscriberExtension.php
@@ -2,21 +2,26 @@
 
 namespace DNADesign\SubsitesDefaultEmails\Extension;
 
+use SilverStripe\Core\Environment;
 use SilverStripe\Core\Extension;
 use SilverStripe\Subsites\Model\Subsite;
 
 /* Overrides the DefaultFrom address for a subsite. */
 
-class EmailExtension extends Extension
+class MailerSubscriberExtension extends Extension
 {
     /**
      * Update $defaultFrom variable if $subsite->DefaultFromEmail has been set
      */
-    public function updateDefaultFrom(&$defaultFrom)
+    public function updateOnMessage($email)
     {
+        $envEmail = Environment::getEnv('SS_EMAIL_SEND_ALL_FROM');
         $subsite = Subsite::currentSubsite();
+
         if ($subsite && trim($subsite->DefaultFromEmail) ?? '') {
-            $defaultFrom = $subsite->DefaultFromEmail;
+            if (!$envEmail || ($envEmail && $subsite->BypassEnvironment)) {
+                $email->setFrom($subsite->DefaultFromEmail);
+            }
         }
     }
 }
